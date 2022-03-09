dbe042581f231216d411fa5b42375a1f1868085c
Briana Ellis-Gibbs <brianaellisgibbs@gmail.com>
2022-03-08 20:22:34 -0500

    final

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-// silence
+const select = document.querySelector(".filter");
+select.addEventListener("change", function (e) {
+    const chosen_food = e.target.value;
+    //everything that has the class item
+    const currently_visible = document.querySelectorAll(".item.active");
+    //this is what you want to see
+  
+    const make_visible = document.querySelectorAll(`.item.${chosen_food}`);
+    
+    // to remove the active class.
+    currently_visible.forEach(function (food) {
+        food.classList.remove("active")
+
+    });
+
+    make_visible.forEach(function (food) {
+        food.classList.add("active")
+
+    });
+
+
+});
+
